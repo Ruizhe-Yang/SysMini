084728fc04597f07e8b1d0ba7c8aaf7050e0b705
Ruizhe-Yang <yang.ruizhe@outlook.com>
2024-12-25 18:11:40 +0800

    '.'

---
+++ org.omg.sysmini/src/org/omg/sysmini/sysml2xmini/util/EpsilonFunction.java
@@ -0,0 +1,13 @@
+package org.omg.sysmini.sysml2xmini.util;
+
+public class EpsilonFunction {
+	
+    public static void main(String[] args) {
+    	run();
+    }
+    
+    public static void run() {
+    	System.out.println("EpsilonFunction running...");
+    }
+    
+}
--- org.omg.sysmini/src/org/omg/sysmini/sysml2xmini/util/SysML2XMIni_file.java
+++ org.omg.sysmini/src/org/omg/sysmini/sysml2xmini/util/SysML2XMIni_file.java
@@ -34,7 +34,7 @@ public class SysML2XMIni_file {
 	//Directory path of the 'sysml.library'.
 	public static String libraryDirectoryPath = "E:\\GitYang\\SysMini\\org.omg.sysmini.runtime\\sysml.library";
 	//File path of the target file 'xxx.sysml'.
-	public static String targetFilePath = "E:\\GitYang\\SysMini\\org.omg.sysmini.runtime\\sysml\\src\\examples\\Packet Example\\PacketUsage.sysml";
+	public static String targetFilePath = "E:\\GitYang\\SysMini\\org.omg.sysmini.runtime\\model\\vehicle example\\VehicleUsages.sysml";
 	//Generate file 'xxx_.sysmlx'.
 	public static String fileName = null;
 	//Directory path of the self.
